7289e7eb9a56158f60b17797be2a683286193571
Mangesh Panchwagh <panchwaghmangesh177@gmail.com>
2025-08-23 11:32:05 +0530

    RestAssured Hello World

---
+++ src/test/java/sample/RestAssuredHelloWorld.java
@@ -0,0 +1,26 @@
+package sample;
+
+import io.restassured.RestAssured;
+
+public class RestAssuredHelloWorld {
+
+	public static void main(String[] args) {
+		
+//		https://restful-booker.herokuapp.com/ping
+		
+		RestAssured.
+			given()
+				.baseUri("https://restful-booker.herokuapp.com")
+			    .basePath("/ping")
+			.when()
+				.log()
+				.all()
+				.get()
+			.then()
+				.log()
+				.all()
+				.statusCode(201);
+			
+			
+	}
+}
